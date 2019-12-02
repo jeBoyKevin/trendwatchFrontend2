f904a5874c3abb4d3a27078b0ec10c96c0daecc0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-02 12:30:18 +0000

    Deploy jeBoyKevin/trendwatchFrontend2 to github.com/jeBoyKevin/trendwatchFrontend2.git:gh-pages

---
+++ precache-manifest.61c32cb15f07d52cdfb8375943ec74a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "759ba5480f1f788352360701c7e09971",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af2d5bce9f3c2b0a5bca",
+    "url": "/static/css/2.653462cc.chunk.css"
+  },
+  {
+    "revision": "2780c3089a1b0dcbe5c9",
+    "url": "/static/css/main.71598800.chunk.css"
+  },
+  {
+    "revision": "af2d5bce9f3c2b0a5bca",
+    "url": "/static/js/2.c7e09fbc.chunk.js"
+  },
+  {
+    "revision": "2780c3089a1b0dcbe5c9",
+    "url": "/static/js/main.1d35c5d5.chunk.js"
+  },
+  {
+    "revision": "f84acfc8c6be1247e129",
+    "url": "/static/js/runtime~main.c195a97c.js"
+  }
+]);
